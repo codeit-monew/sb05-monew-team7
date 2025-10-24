9f1b9057db81850717035d0273e4cc212b51a036
Namsoo315 <gustn2130@naver.com>
2025-10-24 14:28:56 +0900

    fix: 검증 Validation 추가
    기본적인 설정만 함.

--- monew/src/main/java/com/spring/monew/comment/controller/dto/request/CommentRegisterRequest.java
+++ monew/src/main/java/com/spring/monew/comment/controller/dto/request/CommentRegisterRequest.java
@@ -1,9 +1,18 @@
 package com.spring.monew.comment.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record CommentRegisterRequest(
-        UUID articleId,
-        UUID userId,
-        String content
-) {}
+    @NotNull(message = "기사 ID는 필수입니다")
+    UUID articleId,
+
+    @NotNull(message = "사용자 ID는 필수입니다")
+    UUID userId,
+
+    @NotBlank(message = "댓글 내용은 필수입니다")
+    String content
+) {
+
+}
