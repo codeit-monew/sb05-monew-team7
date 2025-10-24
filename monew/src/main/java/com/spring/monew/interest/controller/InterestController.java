963294fabc26ea8381fbf170ef79af9ce2191c0e
Namsoo315 <gustn2130@naver.com>
2025-10-24 16:36:24 +0900

    feat: Controller단 뼈대 구현
    관심사, 구독, 댓글, 좋아요 뼈대 구현

---
+++ monew/src/main/java/com/spring/monew/interest/controller/InterestController.java
@@ -0,0 +1,42 @@
+package com.spring.monew.interest.controller;
+
+import com.spring.monew.interest.controller.dto.response.CursorPageResponseInterestDto;
+import com.spring.monew.interest.controller.dto.response.InterestDto;
+import com.spring.monew.interest.service.InterestService;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/interests")
+@RequiredArgsConstructor
+public class InterestController {
+
+  private final InterestService interestService;
+
+  @PostMapping
+  public InterestDto interestAdd() {
+    return null;
+  }
+
+  @GetMapping
+  public CursorPageResponseInterestDto interestList() {
+    return null;
+  }
+
+  @PatchMapping("/{interestId}")
+  public InterestDto interestModify(@PathVariable UUID interestId) {
+    return null;
+  }
+
+  @DeleteMapping("/{interestId}")
+  public void interestRemove(@PathVariable UUID interestId) {
+
+  }
+}
