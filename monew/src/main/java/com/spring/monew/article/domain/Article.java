9fd95a79b6886e1011a0a90c3bb6e708760fa3d2
yuhandemian <yuhn1011@naver.com>
2025-10-24 15:33:22 +0900

    feat: article, article view entity 구현

---
+++ monew/src/main/java/com/spring/monew/article/domain/Article.java
@@ -0,0 +1,72 @@
+package com.spring.monew.article.domain;
+
+//import com.spring.monew.interest.domain.Interest;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "articles")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE articles SET is_deleted = true, deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@Where(clause = "is_deleted = false") // 조회 시 기본적으로 삭제되지 않은 것만 조회
+public class Article {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "interest_id", nullable = false)
+//    private Interest interest;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "source", nullable = false)
+    private ArticleSource source;
+
+    @Column(name = "source_url", nullable = false, unique = true, length = 255)
+    private String sourceUrl;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "publish_date", nullable = false)
+    private Instant publishDate;
+
+    @Column(name = "summary", nullable = false, columnDefinition = "TEXT")
+    private String summary;
+
+    @Column(name = "comment_count", nullable = false)
+    @ColumnDefault("0")
+    private long commentCount = 0L;
+
+    @Column(name = "view_count", nullable = false)
+    @ColumnDefault("0")
+    private long viewCount = 0L;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @Column(name = "is_deleted", nullable = false)
+    @ColumnDefault("false")
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+}
