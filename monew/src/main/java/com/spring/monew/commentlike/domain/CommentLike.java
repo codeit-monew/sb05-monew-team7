00a48b0706e60a3d241e85e606e0e895c4265483
Namsoo315 <gustn2130@naver.com>
2025-10-24 14:41:51 +0900

    fix: 엔티티 수정
    default 수정 및 GenerationType.UUID 추가

--- monew/src/main/java/com/spring/monew/commentlike/domain/CommentLike.java
+++ monew/src/main/java/com/spring/monew/commentlike/domain/CommentLike.java
@@ -2,6 +2,8 @@ package com.spring.monew.commentlike.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.Instant;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class CommentLike {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.UUID)   //PK 자동 삽입
   private UUID id;
 
   @Column(name = "content_id", nullable = false)
