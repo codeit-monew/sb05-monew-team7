118df3848b9f1c663daf07a29cd25cab0a0367e8
yuhandemian <yuhn1011@naver.com>
2025-10-24 15:11:30 +0900

    fix: article 태일 DDL 오타 수정 view_count

--- monew/src/main/resources/schema.sql
+++ monew/src/main/resources/schema.sql
@@ -58,7 +58,7 @@ CREATE TABLE articles
     publish_date  TIMESTAMPTZ      NOT NULL,
     summary       TEXT             NOT NULL,
     comment_count BIGINT           NOT NULL DEFAULT 0,
-    article_count BIGINT           NOT NULL DEFAULT 0,
+    view_count    BIGINT           NOT NULL DEFAULT 0,
     created_at    TIMESTAMPTZ      NOT NULL,
     updated_at    TIMESTAMPTZ      NOT NULL,
     is_deleted    BOOLEAN          NOT NULL DEFAULT FALSE,
