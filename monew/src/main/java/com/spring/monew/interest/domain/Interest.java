d2dadf8e4d3725a202130d616872b52e798bba79
Namsoo315 <gustn2130@naver.com>
2025-10-24 14:29:54 +0900

    fix: default 수정 및 converter 사용
    default에 대한 값 삽입 및 converter 적용

--- monew/src/main/java/com/spring/monew/interest/domain/Interest.java
+++ monew/src/main/java/com/spring/monew/interest/domain/Interest.java
@@ -1,11 +1,15 @@
 package com.spring.monew.interest.domain;
 
+import com.spring.monew.common.converter.KeywordsConverter;
 import jakarta.persistence.*;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name = "interests")
@@ -19,9 +23,12 @@ public class Interest {
     @Column(nullable = false, unique = true)
     private String name;
 
+    @Convert(converter = KeywordsConverter.class)
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String keywords;
+    private List<String> keywords;
 
     @Column(name = "subscriptions_count", nullable = false)
+    @ColumnDefault("0")
+    @Default
     private long subscriptionsCount = 0L;
 }
