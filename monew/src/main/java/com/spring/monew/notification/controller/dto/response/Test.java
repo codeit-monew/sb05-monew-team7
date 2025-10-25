d20d653168d4b95b0bd33c5727e0e14a745eb8ec
kimyumin03 <alsfla03@gmail.com>
2025-10-25 15:33:54 +0900

    fix: 응답 필드 수정

--- monew/src/main/java/com/spring/monew/notification/controller/dto/response/Test.java
+++
@@ -1,5 +0,0 @@
-package com.spring.monew.notification.controller.dto.response;
-
-public class Test {
-
-}
--- monew/src/main/java/com/spring/monew/notification/controller/test.java
+++ monew/src/main/java/com/spring/monew/notification/controller/test.java
@@ -1,5 +1,5 @@
 package com.spring.monew.notification.controller;
 
-public class Test {
+public class test {
 
 }
