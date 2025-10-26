684e740332e9b53f43f81558e4ac23e2afc2839b
Namsoo315 <gustn2130@naver.com>
2025-10-26 23:14:46 +0900

    fix: ManyToOne 참조 추가
    user, interest 관계 추가

--- monew/src/main/java/com/spring/monew/subscription/domain/Subscription.java
+++ monew/src/main/java/com/spring/monew/subscription/domain/Subscription.java
@@ -1,10 +1,15 @@
 package com.spring.monew.subscription.domain;
 
+import com.spring.monew.interest.domain.Interest;
+import com.spring.monew.user.domain.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
@@ -22,11 +27,14 @@ public class Subscription {
     @GeneratedValue(strategy = GenerationType.UUID)   //PK 자동 삽입
     private UUID id;
 
-    @Column(name = "user_id", nullable = false)
-    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-    @Column(name = "interest_id", nullable = false)
-    private UUID interestId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interest_id", nullable = false)
+    private Interest interest;
 
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
