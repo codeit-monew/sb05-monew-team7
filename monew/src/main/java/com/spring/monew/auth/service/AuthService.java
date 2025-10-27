0be13dbd8f7f611a595f5d25f2cd26a0d2911b26
Haram Jo <harramaram@gmail.com>
2025-10-27 15:02:53 +0900

    refactor: 동일 오류 메세지 반환하도록 변경하였습니다.

--- monew/src/main/java/com/spring/monew/auth/service/AuthService.java
+++ monew/src/main/java/com/spring/monew/auth/service/AuthService.java
@@ -21,10 +21,10 @@ public class AuthService {
   public UserDto login(UserLoginRequest request) {
 
     User user = userRepository.findByEmail(request.email())
-        .orElseThrow(() -> new IllegalArgumentException("이메일이 일치하지 않습니다."));
+        .orElseThrow(() -> new IllegalArgumentException("이메일 또는 비밀번호가 일치하지 않습니다."));
 
     if (!passwordEncoder.matches(request.password(), user.getPassword())) {
-      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+      throw new IllegalArgumentException("이메일 또는 비밀번호가 일치하지 않습니다.");
     }
 
     // Controller로 반환
