2979840a5f93824028b086bf02ff01fd37ec9e71
Namsoo315 <gustn2130@naver.com>
2025-10-27 15:23:54 +0900

    feat: 레이어층 CRUD 구현
    Controller Service Repository 구현 완료

--- monew/src/main/java/com/spring/monew/commentlike/service/impl/CommentLikeServiceImpl.java
+++ monew/src/main/java/com/spring/monew/commentlike/service/impl/CommentLikeServiceImpl.java
@@ -1,8 +1,18 @@
 package com.spring.monew.commentlike.service.impl;
 
+import com.spring.monew.comment.domain.Comment;
+import com.spring.monew.comment.repository.CommentRepository;
 import com.spring.monew.commentlike.controller.dto.response.CommentLikeDto;
+import com.spring.monew.commentlike.domain.CommentLike;
 import com.spring.monew.commentlike.repository.CommentLikeRepository;
 import com.spring.monew.commentlike.service.CommentLikeService;
+import com.spring.monew.interest.domain.Interest;
+import com.spring.monew.subscription.domain.Subscription;
+import com.spring.monew.user.domain.User;
+import com.spring.monew.user.repository.UserRepository;
+import java.time.Instant;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +23,50 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentLikeServiceImpl implements CommentLikeService {
 
   private final CommentLikeRepository commentLikeRepository;
+  private final CommentRepository commentRepository;
+  private final UserRepository userRepository;
 
   @Override
-  public CommentLikeDto addCommentLike() {
-    return null;
+  @Transactional
+  public CommentLikeDto addCommentLike(UUID commentId, UUID userId) {
+    Comment comment = commentRepository.findById(commentId).orElseThrow(
+        () -> new NoSuchElementException("존재하지 않는 댓글 입니다."));
+
+    User user = userRepository.findById(userId).orElseThrow(
+        () -> new NoSuchElementException("존재하지 않는 유저 입니다.")
+    );
+
+    if(commentLikeRepository.existsByComment_IdAndUser_Id(commentId, userId)){
+      throw new IllegalArgumentException("이미 존재하는 좋아요 입니다.");
+    }
+
+    comment.incrementLikeCount();
+
+    CommentLike commentLike = commentLikeRepository.save(new CommentLike(comment, user));
+
+    return new CommentLikeDto(
+        commentLike.getId(),
+        commentLike.getUser().getId(),
+        commentLike.getCreatedAt(),
+        comment.getId(),
+        comment.getArticle().getId(),
+        comment.getUser().getId(),
+        comment.getUser().getNickname(),
+        comment.getContent(),
+        comment.getLikeCount(),
+        comment.getCreatedAt()
+    );
   }
 
   @Override
-  public void removeCommentLike() {
+  @Transactional
+  public void removeCommentLike(UUID commentId, UUID userId) {
+    CommentLike commentLike = commentLikeRepository.findByComment_IdAndUser_Id(commentId,
+            userId)
+        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 좋아요 입니다"));
 
+    commentLike.getComment().decrementLikeCount();
+
+    commentLikeRepository.delete(commentLike);
   }
 }
