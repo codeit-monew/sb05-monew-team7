6c0eca7f631b7ffcaf400f6315cc670bcee3c26f
Haram Jo <harramaram@gmail.com>
2025-10-27 12:53:09 +0900

    feat: 로그인 구현 Controller & Service 있는 구조 중간 커밋

--- monew/src/main/java/com/spring/monew/user/repository/UserRepository.java
+++ monew/src/main/java/com/spring/monew/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.spring.monew.user.repository;
 
 import com.spring.monew.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
+
+  Optional<User> findByEmail(String email);
 }
