41d997825fe50e996dc75615e79df23e7f016494
Haram Jo <harramaram@gmail.com>
2025-10-29 07:05:26 +0900

    chore: 주석 오타 수정

--- monew/src/main/java/com/spring/monew/user/controller/dto/request/UserUpdateRequest.java
+++ monew/src/main/java/com/spring/monew/user/controller/dto/request/UserUpdateRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
     @NotBlank(message = "닉네임은 필수입니다.")
-    @Size(min = 1, max = 20, message = "닉네임은 1~20자 사이여아 합니다." )
+    @Size(min = 1, max = 20, message = "닉네임은 1~20자 사이여야 합니다." )
     String nickname
 ){}
