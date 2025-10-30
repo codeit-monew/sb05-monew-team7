a358d7151568703eebdf27aebf105ddf07ae19e5
kimyumin03 <alsfla03@gmail.com>
2025-10-30 15:23:58 +0900

    fix: article 롤백

--- monew/src/main/java/com/spring/monew/article/repository/ArticleRepository.java
+++ monew/src/main/java/com/spring/monew/article/repository/ArticleRepository.java
@@ -3,11 +3,7 @@ package com.spring.monew.article.repository;
 import com.spring.monew.article.domain.Article;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface ArticleRepository extends JpaRepository<Article, UUID>, ArticleRepositoryCustom {
-
-  @Query("select a.title from Article a where a.id = :id")
-  String findTitleOnlyById(@Param("id") UUID id);
 }
