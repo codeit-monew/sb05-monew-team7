bfb971beb5776e6b6653c680dec1c5439d88cacf
kimyumin03 <alsfla03@gmail.com>
2025-10-30 14:08:41 +0900

    docs: 파일 삭제

--- monew/src/main/java/com/spring/monew/activity/controller/dto/data/Test.java
+++
@@ -1,5 +0,0 @@
-package com.spring.monew.activity.controller.dto.data;
-
-public class Test {
-
-}
--- monew/src/main/java/com/spring/monew/activity/controller/dto/request/test.java
+++
@@ -1,5 +0,0 @@
-package com.spring.monew.activity.controller.dto.request;
-
-public class test {
-
-}
