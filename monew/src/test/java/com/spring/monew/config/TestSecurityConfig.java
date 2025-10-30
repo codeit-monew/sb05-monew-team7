76740949571c1e5c89f45d0ce193fba36e45f60c
Namsoo315 <gustn2130@naver.com>
2025-10-30 18:37:06 +0900

    feat: TestConfig 추가
    테스트 상황에선 SecurityConfig 사용

---
+++ monew/src/test/java/com/spring/monew/config/TestSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.spring.monew.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class TestSecurityConfig {
+
+
+    // 테스트 상황에선 모두 허용
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // ✅ 모든 요청 허용
+        return http.build();
+    }
+}
