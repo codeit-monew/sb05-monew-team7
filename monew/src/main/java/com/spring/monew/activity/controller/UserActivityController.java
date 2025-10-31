8c8df047cfc2fcf2e335de842eec6d2d6e688b1c
kimyumin03 <alsfla03@gmail.com>
2025-10-31 12:04:03 +0900

    fix: controller 수정

--- monew/src/main/java/com/spring/monew/activity/controller/UserActivityController.java
+++ monew/src/main/java/com/spring/monew/activity/controller/UserActivityController.java
@@ -3,6 +3,7 @@ package com.spring.monew.activity.controller;
 import com.spring.monew.activity.controller.dto.response.UserActivityDto;
 import com.spring.monew.activity.service.UserActivityService;
 import com.spring.monew.common.util.RequestUserExtractor;
+import io.swagger.v3.oas.annotations.Hidden;
 import java.security.Principal;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,7 @@ public class UserActivityController {
   private final RequestUserExtractor userExtractor;
 
   // 1) 내 활동내역 (헤더 없으면 401)
+  @Hidden
   @GetMapping("/api/user-activities/me")
   public ResponseEntity<UserActivityDto> myActivity(Principal principal) {
     UUID userId = userExtractor.extractUserId(principal);
