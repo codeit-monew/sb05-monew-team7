46146f7babc8619bdb6c3dc868f9b2ba85d78e06
kimyumin03 <alsfla03@gmail.com>
2025-10-31 09:12:57 +0900

    fix: 테스트 mock 추가(한 줄 추가)

--- monew/src/test/java/com/spring/monew/subscription/service/SubscriptionIntegrationTest.java
+++ monew/src/test/java/com/spring/monew/subscription/service/SubscriptionIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.spring.monew.activity.repository.ActivitySyncRepository;
 import com.spring.monew.interest.domain.Interest;
 import com.spring.monew.interest.repository.InterestRepository;
 import com.spring.monew.subscription.domain.Subscription;
@@ -17,9 +18,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,6 +43,8 @@ class SubscriptionIntegrationTest {
   InterestRepository interestRepository;
   @Autowired
   SubscriptionRepository subscriptionRepository;
+  @Mock
+  ActivitySyncRepository activitySyncRepository;
 
   @Test
   @DisplayName("구독 등록 성공")
