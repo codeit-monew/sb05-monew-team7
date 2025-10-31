332b1b5eeb3b98601cddb428094b728e33308858
kimyumin03 <alsfla03@gmail.com>
2025-10-31 14:40:40 +0900

    feat: 스냅샷 필드 정리

---
+++ monew/src/main/java/com/spring/monew/activity/domain/ActivityCommentDoc.java
@@ -0,0 +1,28 @@
+package com.spring.monew.activity.domain;
+
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("activity_comments")
+@CompoundIndexes({
+    @CompoundIndex(name = "uid_created_desc", def = "{userId:1, createdAt:-1, _id:-1}")
+})
+public class ActivityCommentDoc {
+  @Id private String id;
+  private UUID userId;
+  private UUID articleId;
+  private String articleTitle;
+  private String content;
+  private long likeCount;
+  private Instant createdAt;
+}
---
+++ monew/src/main/java/com/spring/monew/activity/domain/ActivityCommentLikeDoc.java
@@ -0,0 +1,33 @@
+package com.spring.monew.activity.domain;
+
+
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("activity_comment_likes")
+@CompoundIndexes({
+    @CompoundIndex(name = "uid_created_desc", def = "{userId:1, createdAt:-1, _id:-1}")
+})
+public class ActivityCommentLikeDoc {
+  @Id private String id; // likeEventId
+  private UUID userId; // likedBy
+  private UUID commentId;
+  private UUID articleId;
+  private String articleTitle;
+  private UUID commentUserId;
+  private String commentUserNickname;
+  private String commentContent;
+  private long commentLikeCount;
+  private Instant commentCreatedAt;
+  private Instant createdAt; // like time
+}
