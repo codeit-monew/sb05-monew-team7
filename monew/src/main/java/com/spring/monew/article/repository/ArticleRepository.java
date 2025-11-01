87570a17f0e92a1ab9eb98593196cb385819769c
Namsoo315 <gustn2130@naver.com>
2025-11-01 17:54:58 +0900

    fix: UserActivityQueryRepository 구현
    활동내역 MongoDB 저장

--- monew/src/main/java/com/spring/monew/article/repository/ArticleRepository.java
+++ monew/src/main/java/com/spring/monew/article/repository/ArticleRepository.java
@@ -1,8 +1,15 @@
 package com.spring.monew.article.repository;
 
 import com.spring.monew.article.domain.Article;
+import java.util.Collection;
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, UUID>, ArticleRepositoryCustom {
+
+  @Query("select a.sourceUrl from Article a where a.sourceUrl in :urls")
+  Set<String> findExistingSourceUrls(@Param("urls") Collection<String> urls);
 }
