f22e2bbbc91f2aeb013ffbf9b1b426aa2b5ac54b
kimyumin03 <alsfla03@gmail.com>
2025-11-02 15:43:05 +0900

    fix: profile 수정

--- monew/src/main/java/com/spring/monew/common/config/MongoConfig.java
+++ monew/src/main/java/com/spring/monew/common/config/MongoConfig.java
@@ -1,7 +1,6 @@
 package com.spring.monew.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
