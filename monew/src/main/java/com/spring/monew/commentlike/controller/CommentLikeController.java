b92525f23da90ad5b68e1daf26bc20060c99b128
kimyumin03 <alsfla03@gmail.com>
2025-11-03 11:03:55 +0900

    feat: 좋아요 관련 swagger 명세

--- monew/src/main/java/com/spring/monew/commentlike/controller/CommentLikeController.java
+++ monew/src/main/java/com/spring/monew/commentlike/controller/CommentLikeController.java
@@ -7,6 +7,10 @@ import com.spring.monew.commentlike.service.CommentLikeService;
 import com.spring.monew.common.util.RequestUserExtractor;
 import com.spring.monew.notification.domain.NotificationResourceType;
 import com.spring.monew.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.security.Principal;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
+@Tag(name = "댓글 좋아요 관리", description = "댓글 좋아요 관련 API")
 public class CommentLikeController {
   private final CommentLikeService commentLikeService;
   private final RequestUserExtractor userExtractor;
   private final UserActivityService userActivityService;
   private final NotificationService notificationService;
 
+  @Operation(summary = "댓글 물리 삭제", description = "댓글 좋아요를 등록합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "댓글 좋아요 성공"),
+      @ApiResponse(responseCode = "404", description = "댓글 정보 없음"),
+      @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+  })
   @PostMapping("/{commentId}/comment-likes")
   public CommentLikeDto commentLikeAdd(@PathVariable UUID commentId,
     Principal principal){
@@ -50,6 +61,12 @@ public class CommentLikeController {
     );
   }
 
+  @Operation(summary = "댓글 좋아요 취소", description = "댓글 좋아요를 취소합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "댓글 좋아요 취소 성공"),
+      @ApiResponse(responseCode = "404", description = "관심사 정보 없음"),
+      @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+  })
   @DeleteMapping("/{commentId}/comment-likes")
   public void commentLikeDelete(@PathVariable UUID commentId,
       Principal principal){
