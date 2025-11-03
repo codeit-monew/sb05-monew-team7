1feb492020ff0720e92905c5ae6af7a0f2fbcdf9
yuhandemian <yuhn1011@naver.com>
2025-11-03 18:21:48 +0900

    refactor: 주석 추가

--- monew/src/main/java/com/spring/monew/auth/config/SecurityConfig.java
+++ monew/src/main/java/com/spring/monew/auth/config/SecurityConfig.java
@@ -23,28 +23,32 @@ public class SecurityConfig {
   private final RequestIdFilter requestIdFilter;
 
   @Value("${monitoring.prometheus.allow-ip}")
-  private String prometheusAllowIp;
+  private String prometheusAllowIp;// yml 속성 주입 (기본값은 localhost)
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)// CSRF 보안 비활성화 (개발용)
 
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers("/actuator/prometheus")
             .access((authentication, context) -> {
               String remoteAddr = context.getRequest().getRemoteAddr();
-              boolean equals = remoteAddr.equals(prometheusAllowIp);
+              boolean equals = remoteAddr.equals(prometheusAllowIp);// Prometheus IP
               return new AuthorizationDecision(equals);
             })
             .requestMatchers("/actuator/health", "/actuator/info",
-                "/actuator/loggers").permitAll()
+                "/actuator/loggers").permitAll()//Actuator 허용 (원래는 이렇게 하면 안됨)
+                //  모든 경로 허용 (개발용)
             .requestMatchers("/api/batch/**").permitAll()
             .anyRequest().permitAll()
         )
+            // 헤더에 담긴 userId를 읽어서 인증 정보를 만들어주는 필터
         .addFilterBefore(requestIdFilter, UsernamePasswordAuthenticationFilter.class)
         .addFilterBefore(headerAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+      //.requestMatchers(HttpMethod.POST, "/api/users", "/api/users/login").permitAll()
+      //  나머지 요청은 인증 필요
+      //.anyRequest().authenticated()
     return http.build();
   }
 
