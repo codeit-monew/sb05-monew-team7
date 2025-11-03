73ee84463e453ed543f8ee93b0d68cc425a48e3a
yuhandemian <yuhn1011@naver.com>
2025-11-03 17:14:57 +0900

    feat: ArticleRepository에 삭제 관련 쿼리 추가
    - findAllByDeletedAtIsNotNull: 삭제된 기사 조회 (백업용)
    - hardDeleteById: 물리적 삭제 쿼리
    - restoreById: 소프트 삭제 복원 쿼리

--- monew/src/main/java/com/spring/monew/article/repository/ArticleRepository.java
+++ monew/src/main/java/com/spring/monew/article/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.spring.monew.article.repository;
 
 import com.spring.monew.article.domain.Article;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +14,17 @@ public interface ArticleRepository extends JpaRepository<Article, UUID>, Article
 
   @Query("select a.sourceUrl from Article a where a.sourceUrl in :urls")
   Set<String> findExistingSourceUrls(@Param("urls") Collection<String> urls);
+
+  @Modifying(clearAutomatically = true)
+  @Query(value = "DELETE FROM articles WHERE id = :articleId", nativeQuery = true)
+  void hardDelete(@Param("articleId") UUID articleId);
+
+  @Query(value = "SELECT EXISTS(SELECT 1 FROM articles WHERE id = :articleId)", nativeQuery = true)
+  boolean existsIncludingDeleted(@Param("articleId") UUID articleId);
+
+  @Query(value = "SELECT * FROM articles WHERE id = :articleId", nativeQuery = true)
+  Optional<Article> findIncludingDeleted(@Param("articleId") UUID articleId);
+
+  @Query("SELECT a.sourceUrl FROM Article a")
+  Set<String> findAllSourceUrls();
 }
